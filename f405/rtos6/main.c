b82e14dacd4176364fd91100cc76afe9df429842
u262d <nergej.seganov@proton.me>
2025-07-06 19:05:31 +0200

    prep for dma

--- f405/rtos6/main.c
+++ f405/rtos6/main.c
@@ -1,8 +1,11 @@
 #include "hal.h"
 
 //uint8_t send[] = "sending\r\n";
+static void task_spkr(void *args __attribute((unused))){
+while (1){gpio_toggle(PIN('C',2));vTaskDelay(pdMS_TO_TICKS(2));/*500hz*/ }
+}
 static void task0(void *args __attribute((unused))){
-while (1){gpio_toggle(PIN('B',2));vTaskDelay(pdMS_TO_TICKS(200)); }
+while (1){gpio_toggle(PIN('B',2));vTaskDelay(pdMS_TO_TICKS(200));/**/ }
 }
 int main(void){
 clock_init_hse();
@@ -12,11 +15,13 @@ printf("clock init done!!!\r\n");
 //gpio_output(PIN('B',5));
 gpio_init(PIN('B',5), GPIO_MODE_OUTPUT,GPIO_OTYPE_PUSH_PULL,GPIO_SPEED_LOW,GPIO_PULL_NONE,0);
 gpio_output(PIN('B',2));
+gpio_output(PIN('C',2));
 gpio_init(PIN('B',6), GPIO_MODE_INPUT,GPIO_OTYPE_PUSH_PULL,GPIO_SPEED_LOW,GPIO_PULL_UP,0);
 exti_attach();
 //start_tim2();
 
 xTaskCreate(task0,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
+xTaskCreate(task_spkr,"spkr",100,NULL,configMAX_PRIORITIES-1,NULL);
 printf("task0 created\r\n");
 vTaskStartScheduler();//blocks
 //printf("scheduler started\r\n"); wont reach here after
